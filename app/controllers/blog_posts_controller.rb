94caafa2f8c3e77ba36aa72a32e17f6fccc21dfe
Josel Castro <josel12212@gmail.com>
2025-02-20 15:03:45 +0800

    feat: install devise

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,10 @@
 class BlogPostsController < ApplicationController
+    before_action :set_blog_post, only: [ :show, :edit, :update, :destroy ]
     def index
         @blog_posts = BlogPost.all
     end
 
     def show
-        @blog_post = BlogPost.find(params[:id])
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
     end
@@ -28,11 +28,9 @@ class BlogPostsController < ApplicationController
     end
 
     def edit
-        @blog_post = BlogPost.find(params[:id])
     end
 
     def update
-        @blog_post = BlogPost.find(params[:id])
         if @blog_post.update(blog_post_params)
           redirect_to @blog_post
         else
@@ -41,7 +39,6 @@ class BlogPostsController < ApplicationController
     end
 
     def destroy
-        @blog_post = BlogPost.find(params[:id])
         @blog_post.destroy
         redirect_to root_path
     end
@@ -50,5 +47,9 @@ class BlogPostsController < ApplicationController
 
     def blog_post_params
         params.require(:blog_post).permit(:title, :body)
+    end
+
+    def set_blog_post
+        @blog_post = BlogPost.find(params[:id])
     end
 end
